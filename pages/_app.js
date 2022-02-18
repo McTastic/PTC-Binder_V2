ddf807f9febc703128a16498c161553be0a6c00a
Ryan McCarthy <rmmcc92@aol.com>
2022-02-18 12:12:53 -0500

    changed how theme was being imported

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,28 @@
 import "/styles/globals.css";
 import "/styles/pokeBall.css";
 import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import createEmotionCache from "../styles/createEmotionCache";
+import Head from "next/head";
+import theme from "/styles/theme";
 
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Triangle Drive In</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Typography } from "@mui/material";
-import Layout from "../components/layout";
 
 export default function Home() {
   return (
-    <Layout title="Home">
-      <div className="center-on-page">
-        <div className="pokeball">
-          <div className="pokeball__button"></div>
-        </div>
+    <div className="center-on-page">
+      <div className="pokeball">
+        <div className="pokeball__button"></div>
       </div>
-    </Layout>
+    </div>
   );
 }
