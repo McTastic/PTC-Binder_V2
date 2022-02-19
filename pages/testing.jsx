ec839d18eac743c05f4176da9a506003f70b134c
Sean M <mctastic2121@gmail.com>
2022-02-19 14:43:04 -0500

    small package.json change

--- pages/testing.jsx
+++ pages/testing.jsx
@@ -8,7 +8,7 @@ import Typography from "@mui/material/Typography";
 
 export default function ImgMediaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345, margin: "auto" }}>
       <CardMedia
         component="img"
         alt="green iguana"
