69b82c4a4b4f35d759b706f57e559fa884c9ba37
Ryan McCarthy <rmmcc92@aol.com>
2022-02-23 20:21:44 -0500

    initail card component done now we need to test with a larger search

---
+++ components/ResultCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Link,
+  List,
+  ListItem,
+  TextField,
+  useTheme,
+  CircularProgress,
+} from "@mui/material";
+
+export default function ResultCard(props) {
+  return (
+    <>
+      <Card
+        id={`${props.id}-card`}
+        sx={{
+          margin: "auto",
+          width: "14em",
+          height: "19.25em",
+          backgroundColor: "rgba(107, 181, 241, .5)",
+          transition: "opacity .5s, height .5s",
+          "&:hover": {
+            // boxShadow: `2px 2px 20px 5px ${props.aura}`,
+            height: "23em",
+            button: {
+              opacity: "1",
+              display: "block",
+            },
+          },
+        }}
+      >
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          image={props.image}
+          sx={{
+            margin: "auto",
+            padding: "5px",
+          }}
+        />
+        <CardContent sx={{ textAlign: "center" }}>
+          <Button
+            variant="contained"
+            id={`${props.id}-button`}
+            fullWidth
+            sx={{
+              transition: "opacity .5s",
+              opacity: "0",
+            }}
+          >
+            Add
+          </Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
--- pages/testing.jsx
+++ pages/testing.jsx
@@ -12,6 +12,7 @@ import { Store } from "/utils/globalStore";
 import { CircularProgress } from "@mui/material";
 import getConfig from "next/config";
 import theme from "/styles/theme.js";
+import ResultCard from "@components/ResultCard";
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -56,76 +57,63 @@ const ImgMediaCard = () => {
 
   return (
     <>
-      {loading ? (
-        <CircularProgress />
-      ) : error ? (
-        <Typography variant="h6" color="error">
-          {" "}
-          {error}{" "}
-        </Typography>
-      ) : (
-        <Grid container display="flex" justifyContent={"center"}>
-          <Grid item xs={12} md={6} lg={4}>
-            <Card
-              sx={{
-                margin: "auto",
-                width: "14em",
-                height: "19.25em",
-                backgroundColor: "rgba(107, 181, 241, .5)",
-                transition: "opacity .5s, height .5s",
-                "&:hover":{
-                  boxShadow:`2px 2px 20px 5px ${theme.palette.types.grass}`,
-                  height:"23em",
-                  button:{
-                    opacity:"1",
-                    display: "block"
-                  }
-                }
-              }}
-            >
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image={data.data.data.images.large}
-                sx={{
-                  margin: "auto",
-                  padding: "5px",
-                }}
-              />
-              <CardContent sx={{ textAlign:"center"}}>
-                <Button variant="contained" fullWidth 
-                sx={{
-                  transition:"opacity .5s",
-                  opacity: "0"
-                }}
-                >
-                  Add
-                </Button>
-                {/* <Typography
-                  gutterBottom
-                  color="text.secondary"
-                  variant="h5"
-                  component="div"
-                  textAlign="center"
-                >
-                  {data.data.data.name}
-                </Typography> */}
-                {/* <Typography
-                  color="text.secondary"
-                  m={2}
-                  sx={{
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                    textAlign: "center",
-                  }}
-                >
-                  {data.data.data.flavorText ? data.data.data.flavorText : "Nothing Here!"}
-                </Typography> */}
-              </CardContent>
-            </Card>
-          </Grid>
+      <Grid container display="flex" justifyContent={"center"}>
+        <Grid item xs={12} md={6} lg={4}>
+          {loading ? (
+            <CircularProgress />
+          ) : error ? (
+            <Typography variant="h6" color="error">
+              {" "}
+              {error}{" "}
+            </Typography>
+          ) : (
+            <ResultCard
+              id={data.data.data.id}
+              image={data.data.data.images.large}
+              type={data.data.data.type}
+            />
+            // <Card
+            //   sx={{
+            //     margin: "auto",
+            //     width: "14em",
+            //     height: "19.25em",
+            //     backgroundColor: "rgba(107, 181, 241, .5)",
+            //     transition: "opacity .5s, height .5s",
+            //     "&:hover": {
+            //       boxShadow: `2px 2px 20px 5px ${theme.palette.types.grass}`,
+            //       height: "23em",
+            //       button: {
+            //         opacity: "1",
+            //         display: "block",
+            //       },
+            //     },
+            //   }}
+            // >
+            //   <CardMedia
+            //     component="img"
+            //     alt="green iguana"
+            //     image={data.data.data.images.large}
+            //     sx={{
+            //       margin: "auto",
+            //       padding: "5px",
+            //     }}
+            //   />
+            //   <CardContent sx={{ textAlign: "center" }}>
+            //     <Button
+            //       variant="contained"
+            //       fullWidth
+            //       sx={{
+            //         transition: "opacity .5s",
+            //         opacity: "0",
+            //       }}
+            //     >
+            //       Add
+            //     </Button>
+            //   </CardContent>
+            // </Card>
+          )}
         </Grid>
-      )}
+      </Grid>
     </>
   );
 };
