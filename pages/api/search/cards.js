3a24f81966781653f75fd2d2532a55a4e8d856d2
Ryan McCarthy <rmmcc92@aol.com>
2022-02-24 13:45:32 -0500

    multiple cards on testing page

--- pages/api/search/cards.js
+++ pages/api/search/cards.js
@@ -1,15 +1,24 @@
 import nc from "next-connect";
 import axios from "axios";
+import getConfig from "next/config";
+
+const { serverRuntimeConfig } = getConfig();
+const key = serverRuntimeConfig.POKE_KEY;
 
 const handler = nc();
+const userInput = "pikachu";
+const currentPage = 1;
 
 handler.get(async (req, res) => {
   try {
-    const result = await axios.get(`https://api.pokemontcg.io/v2/cards/xy1-1`, {
-      headers: {
-        "x-api-key": process.env.POKE_KEY,
-      },
-    });
+    const result = await axios.get(
+      `https://api.pokemontcg.io/v2/cards/?q=name:${userInput}&page=${currentPage}&pageSize=10`,
+      {
+        headers: {
+          "X-Api-Key": key,
+        },
+      }
+    );
     // console.log(result);
     res.send(result.data);
   } catch (error) {
