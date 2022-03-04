5b33cbad9ba4ebe8768404d48109c869f55c5174
Sean M <mctastic2121@gmail.com>
2022-03-04 15:16:38 -0500

    changed loader to be permanently on the blank card

--- pages/search.jsx
+++ pages/search.jsx
@@ -110,7 +110,6 @@ export default function TextFieldHiddenLabel() {
                     alignItems: "center",
                   }}
                 >
-                  <PokeLoader/>
                 </Grid>
               ) : (
                 <ResultCard
