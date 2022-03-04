643ce7b1eaed5171a7502047f04f3c7472fb9589
Sean M <mctastic2121@gmail.com>
2022-03-03 21:14:42 -0500

    added placeholder text to home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import { Typography, Grid } from "@mui/material";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Grid ml={40}>
+        <Typography variant="h3">
+            Home page coming soon!
+        </Typography>
+    </Grid>
+)
 }
