7294c72af35663d7f5e44a207f99e63d2564afaf
Sean M <mctastic2121@gmail.com>
2022-04-11 15:50:40 -0400

    fixed small about page error

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,9 @@ import { Typography, Grid } from "@mui/material";
 
 export default function About() {
     return (
-        <Grid xs={12} md={6} lg={4} 
+        <Grid 
+        item
+        xs={12} md={6} lg={4} 
         sx={{
             ml:{xs:"2em",sm:"15em",md:"20em",lg:"40em"}
         }}
